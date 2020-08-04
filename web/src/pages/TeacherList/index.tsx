9f8234b89259ef42768ca2bb6fb16b17d9593ec8
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-08-04 16:59:23 -0300

    chore(props.children): add form on header component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -7,7 +7,24 @@ import PageHeader from '../../components/PageHeader';
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Estes são os proffys disponíveis." />
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Dia da Semana</label>
+            <input type="text" id="week-day"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time"/>
+          </div>
+        </form>
+      </PageHeader>
     </div>
   );
 }
