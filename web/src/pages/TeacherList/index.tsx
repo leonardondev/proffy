4ce5cc102c2f949e40f19b1a6a81cdde6bde68a2
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-08-04 18:13:29 -0300

    chore: add teacher listing

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,6 +4,8 @@ import './styles.css';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
@@ -25,6 +27,34 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/19802320?s=460&u=8b3e4cbb9dacb722482982bcf6324b03149c9962&v=4" alt="Leonardo"/>
+            <div>
+              <strong>Leonardo</strong>
+              <span>Química</span>
+            </div>
+          </header>
+          <p>
+            Enthusiast of the best web & mobile development technologies.
+            <br /><br />
+            Passionate about education and changing people's lives through programming. More than 200,000 people have already undergone one of my trainings.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
