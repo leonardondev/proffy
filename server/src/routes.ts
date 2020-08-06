081baf2b8185ad24156fd6c453ad091d60ccff66
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-08-05 22:56:28 -0300

    chore: added connections controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 
 const routes = express.Router();
-const classesControllers = new ClassesController();
+const classesControllers= new ClassesController();
+const connectionController = new ConnectionsController();
 
 
 routes.post('/classes', classesControllers.create);
 routes.get('/classes', classesControllers.index);
+
+routes.post('/connections', connectionController.create);
+routes.get('/connections', connectionController.index);
+
 
 export default routes;
