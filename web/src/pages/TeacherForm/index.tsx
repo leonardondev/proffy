20a3d4e7bc6a2ec8bd33665cd7fd658b06b78956
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-08-06 11:28:43 -0300

    chore: added form on TeacherForm page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -3,11 +3,47 @@ import React from 'react';
 import './styles.css';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas" />
+      <PageHeader
+        title="Que incrível que você quer dar aulas"
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Link da sua foto"/>
+          <Input name="whatsapp" label="Whatsapp"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subject" label="Matéria"/>
+          <Input name="cost" label="Custo da sua hora por aula"/>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
+
+
     </div>
   );
 }
