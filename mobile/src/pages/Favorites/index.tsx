cb0083736133b0f1b59fa29836db6cd63693c2ba
LeonardoFuba <leonardo3.nascimento@gmail.com>
2020-08-08 17:04:02 -0300

    chore: added study tabs components

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles';
 
 function Favorites() {
   return (
     <View style={styles.container}>
+      <PageHeader title="Meus proffys Favoritos" />
 
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
+import TeacherItem from '../../components/TeacherItem';
+import { ScrollView } from 'react-native-gesture-handler';
 
 function TeacherList() {
   return (
     <View style={styles.container}>
+      <PageHeader title="Proffys Disponíveis" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
+
 
     </View>
   );
